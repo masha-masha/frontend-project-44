5b0fa634ebdc6ea01b1f3a132693fdbaf79f4dde
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-05 02:47:19 +0300

    third step

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+const greeting = () => {
+    const playerName = readlineSync.question('May I have your name?')
+    console.log(`${'Hello, '}${ playerName}${'!'}`);
+};
+export default greeting;
