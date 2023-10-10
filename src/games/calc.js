74d4cc9c986c95c27238f950fd967fdfa15586c9
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-10 04:38:05 +0300

    six step create game calc

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,4 +1,29 @@
 import playGame from '../index.js';
-import getNumber from '../src/utils.js';
-const rulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".'
-const number = getNumber;
+import { getNumber, getIndex } from '../utils.js';
+const rulesOfTheGame = 'What is the result of the expression?'
+
+const operators = ['-','+','*'];
+const calculate = (x, y, operator) => {
+    switch (operator) {
+      case '+':
+        return x + y;
+      case '-':
+        return x - y;
+      case '*':
+        return x * y;
+      default:
+        throw new Error(`There is no such operator like '${operator}'!`);
+    }
+  };
+  const playRound = () => {
+    const number1 = getNumber();
+    const number2 = getNumber();
+    const operator = operators[getIndex(operators)];
+    const question = `${number1} ${operator} ${number2}`;
+    const correctAnswer = calculate(number1, number2, operator).toString();
+    return [question, correctAnswer];
+  };
+  
+  const brainCalc = () => playGame(rulesOfTheGame,playRound);
+  export default brainCalc;
+
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 const getNumber = () => Math.round(Math.random() * 100);
 const getIndex = (array) => Math.floor(Math.random() * array.length);
-export {getNumber, getIndex};
+export { getNumber, getIndex };
