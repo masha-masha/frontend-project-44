6a3abee9ec13ff9d2254fd4dcc65c620bcc773be
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-12 07:08:57 +0300

    created game brain-prog

---
+++ bin/brain-progression.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+import brainProg from "../src/games/prog.js";
+ brainProg();
---
+++ src/games/prog.js
@@ -0,0 +1,23 @@
+import playGame from '../index.js';
+import { getNumber, getIndex } from '../utils.js';
+const rulesOfTheGame = 'What number is missing in the progression?';
+const getProgression = (start, length, step) => {
+    const progression = [];
+    for (let i = start; progression.length < length; i += step) {
+      progression.push(i);
+    }
+    return progression;
+  };
+  const playRound= () => {
+    const start = getNumber(0, 100);
+    const length = getNumber(5, 10);
+    const step = getNumber(1, 10);
+    const progression = getProgression(start, length, step);
+    const indexMissingNumber = getIndex(progression);
+    const correctAnswer = progression[indexMissingNumber].toString();
+    progression[indexMissingNumber] = '..';
+    const question = progression.join(' ');
+    return [question, correctAnswer];
+  };
+  const brainProg = () => playGame(rulesOfTheGame, playRound);
+  export default brainProg;
