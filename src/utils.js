0bf8084f9fb61558e701c7b028728effeca0e664
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-12 05:54:17 +0300

    changed func getNumber inside

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getNumber = () => Math.round(Math.random() * 100);
+const getNumber = (min, max) => Math.round(Math.random() * (min - max + 1) + max);
 const getIndex = (array) => Math.floor(Math.random() * array.length);
 export { getNumber, getIndex };
