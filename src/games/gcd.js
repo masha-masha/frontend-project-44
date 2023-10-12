983d29dcd1364f7902bf5b0a9115fba797a412d4
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-12 05:52:29 +0300

    created logic for brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,15 @@
+import playGame from '../index.js';
+import { getNumber } from '../utils.js';
+const rulesOfTheGame = 'Find the greatest common divisor of given numbers.'
+
+const getGcd = (a, b) => ((a % b) ? getGcd(b, a % b) : Math.abs(b));
+
+const playRound = () => {
+    const number1 = getNumber(0,50);
+    const number2 = getNumber(0, 50);
+    const question = `${number1 } ${number2}`;
+    const correctAnswer = getGcd(number1, number2).toString();
+    return [question, correctAnswer];
+  };
+  const brainGcd = () => playGame(rulesOfTheGame,playRound);
+  export default brainGcd;
