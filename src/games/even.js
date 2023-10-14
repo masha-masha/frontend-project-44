e901a93431106ec50362ab737a7fcc8063d49a31
Maria Bazhenova <yankeevsdixie@gmail.com>
2023-10-14 21:42:07 +0300

    fixed the last error for linter

--- src/games/even.js
+++ src/games/even.js
@@ -4,10 +4,10 @@ import { getNumber } from '../utils.js';
 
 const rulesOfTheGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 const playRound = () => {
-  const number = getNumber(0, 50);
-  const question = number;
+  const num = getNumber(0, 50);
+  const question = num;
   const isEven = (number) => !(number % 2);
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const correctAnswer = isEven(num) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 const brainEven = () => playGame(rulesOfTheGame, playRound);
